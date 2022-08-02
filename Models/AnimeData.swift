ccce34dd8ec04eb9d5a330be96551ab5eb94423a
thinn <thinnphm826@gmail.com>
2022-08-02 12:40:59 +0700

    8/2 - Update AnimeData

---
+++ Models/AnimeData.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+var animes : [Anime] = [
+    Anime(name: "", genre: " ", producer: " "),
+    Anime(name: "", genre: " ", producer: " "),
+    Anime(name: "", genre: " ", producer: " "),
+    Anime(name: "", genre: " ", producer: " "),
+    Anime(name: "", genre: " ", producer: " "),
+    Anime(name: "", genre: " ", producer: " "),
+    Anime(name: "", genre: " ", producer: " "),
+    Anime(name: "", genre: " ", producer: " "),
+    Anime(name: "", genre: " ", producer: " ")
+]
